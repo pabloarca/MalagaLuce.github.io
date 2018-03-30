3205d06b2ee08145ca7ef912fa03d2bc2a02f715
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-03-30 21:40:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -218,7 +218,7 @@ else {
       let properties = e['features'][0]['properties'],
           geometry = e['features'][0]['geometry']
           html = `<h3>${ properties['finishedDate'] ? properties['finishedDate'] : ''}</h3>
-               <h3>${properties['volunteers'] ? properties['volunteers'] : ''}<h3>`;
+               <h3>${properties['volunteers'] ? properties['name'] : ''}<h3>`;
 
       map.flyTo({
         center: geometry['coordinates'],
