5e3746ce7e7a7254a41f3cb1d45966406c7115ba
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-03-30 21:36:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -217,7 +217,7 @@ else {
     map.on('click', 'action-points', e => {
       let properties = e['features'][0]['properties'],
           geometry = e['features'][0]['geometry']
-          html = `<h3>${ properties['id'] ? properties['id'] : properties['use']}</h3>
+          html = `<h3>${ properties['finishedDate'] ? properties['finishedDate'] : ''}</h3>
                ${properties['ocupation'] ? '<span>Ocupación del parking <meter low="50" high="75" max="100" value="80"></meter></span>' : ''}`;
 
       map.flyTo({
