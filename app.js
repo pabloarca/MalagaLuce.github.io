309cc62112b3ea2896eee3baf83041934ed26a21
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-03-31 22:45:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -218,8 +218,8 @@ else {
       let properties = e['features'][0]['properties'],
           geometry = e['features'][0]['geometry']      
                                 
-          html = `<h3>Realizado en fecha: ${ properties['id'] ? properties['finishedDate'] : ''}</h3>            
-                  <h2>Antes: </h2><img src="${properties['id'] ? properties['creationPic'] : ''}">
+          html = `<h3 class="popupTitle">Realizado en fecha: ${ properties['id'] ? properties['finishedDate'] : ''}</h3>            
+                  <h2 >Antes: </h2><img src="${properties['id'] ? properties['creationPic'] : ''}">
                   <h2>Después: </h2><img src="${properties['id'] ? properties['finishedPic'] : ''}">`;
 
       
