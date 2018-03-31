473a0b2ccb2d6d3b4952b6827a3155d6397f693f
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-03-31 21:31:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -216,13 +216,10 @@ else {
 
     map.on('click', 'action-points', e => {
       let properties = e['features'][0]['properties'],
-          geometry = e['features'][0]['geometry']
-          
-          voluntarios = properties.forEach(function(name){
-                                });                                      
-    
+          geometry = e['features'][0]['geometry']      
+                                
           html = `<h3>Realizado en fecha: ${ properties['finishedDate'] ? properties['finishedDate'] : ''}</h3>
-            ${properties['id'] ? voluntarios : ''}`;
+            ${properties['id'] ? properties['finishedPic'] : ''}`;
 
       map.flyTo({
         center: geometry['coordinates'],
