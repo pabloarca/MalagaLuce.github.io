74644202684e0459bc48f6712c8745996005e882
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-03-31 20:29:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -216,8 +216,8 @@ else {
 
     map.on('click', 'action-points', e => {
       let properties = e['features'][0]['properties'],
-          geometry = e['features'][0]['geometry'],
-          volunteers = e[
+          geometry = e['features'][0]['geometry']
+         
           html = `<h3>Realizado en fecha: ${ properties['finishedDate'] ? properties['finishedDate'] : ''}</h3>
                ;
 
